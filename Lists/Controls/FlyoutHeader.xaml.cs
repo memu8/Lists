0420ef857ccd5378f7d2fd3cfcf15fc4a572228e
memu8 <66331238+memu8@users.noreply.github.com>
2020-06-04 12:00:26 -0500

    initialized git repo

---
+++ Lists/Controls/FlyoutHeader.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Lists.Controls
+{
+    public partial class FlyoutHeader : ContentView
+    {
+        public FlyoutHeader()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Lists/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Lists.Models;
+
+namespace Lists.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
